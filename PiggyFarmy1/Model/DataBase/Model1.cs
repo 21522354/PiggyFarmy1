7b359006db01ce031efb6dbe32441d66787f9de4
21522354 <120326395+21522354@users.noreply.github.com>
2023-10-13 15:44:49 +0700

    Kết nối sql database

---
+++ PiggyFarmy1/Model/DataBase/Model1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace PiggyFarmy1.Model.DataBase
+{
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=PiggyFarmyEntities")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
